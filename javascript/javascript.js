694abc1c6279aebad5801fa8bb7afea97475c56b
Moisur <mosiurislamwebdesign@gmail.com>
2022-02-17 17:09:21 +0600

    javascript-complid

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -27,56 +27,32 @@ function calculate(){
         alert("Please positive number")
     }       
 }
+// sava control 
+function saveoffer(){
+    const TotalExpenses = document.getElementById("total-expenses").innerText;
+    const TotalExpensestype = parseFloat(TotalExpenses);
+    const TotalBalance =document.getElementById("total-Balance").innerText;
+    const TotalBalancetype = parseFloat(TotalBalance);
+    // add  TotalExpensestype and  TotalBalancetype, totalincome
+    const totalincome = (TotalExpensestype + TotalBalancetype);
+    // save input  function 
+    const saveinput = input("save-input");
+    if(saveinput > 0){
+        const total = (totalincome * saveinput)/100;
+        document.getElementById("Saving-Amount").innerText = total;
+        const result = TotalBalancetype-total;
+        if(result > total){
+            document.getElementById("Remaining").innerText = result;
+        }
+        else{
+            alert("sorry, Remaining Balance Must be big")
+        }  
+    }
+    else if(isNaN(saveinput)){
+        alert("Please positive number")
+    }
+    else{
+        alert("Please positive number")
+    }
+}
 
-
-
-
-
-
-
-
-//  calculate
-// function input(inputid){
-//     const inputNumber = document.getElementById(inputid);
-//     const inputNumbervalue = parseFloat(inputNumber.value);
-//     // inputNumber.value ='';
-//     return inputNumbervalue;
-// }
-// function input(){
-//     const inputNumber = document.getElementById("incam-value");
-//     return inputNumber;
-// }
-// function calculate(){
-//     const inputvalue =input("incam-value");
-//     // const Foodvalue =input("Food");
-//     // const Redvalue =input("Red");
-//     // const Chothervalue =input("Chother");
-//     // const Expenses =Foodvalue+Redvalue+Chothervalue;
-//     console.log(inputvalue)
-    // if(inputvalue > 0 || Foodvalue > 0 || Redvalue > 0 || Chothervalue > 0 ){
-    //     if(inputvalue> Expenses){
-    //         const TotalExpenses = document.getElementById("Total-Expenses").innerText =Expenses;
-    //         document.getElementById("totalBalance").innerText =inputNumber-TotalExpenses
-    //     }
-    //     else{
-    //         alert("boro number")
-    //     }
-    // }
-    // else if(isNaN(inputvalue) || isNaN(Foodvalue) || isNaN(Redvalue)|| isNaN(Chothervalue)){
-    //     alert("dekhi number")
-    // }    
-// }
-// function savedata(){
-//     const TotalExpenses = document.getElementById("Total-Expenses").innerText;
-//     const TotalExpensestype = parseFloat(TotalExpenses);
-//     const totalBalancevalue = document.getElementById("totalBalance").innerText;
-//     const totalBalancetype = parseFloat(totalBalancevalue);
-//     const totalBalancevalueresultvalo =(totalBalancetype + TotalExpensestype) 
-
-//     // 
-//      const saveinput = input("save-input");
-//      const avingAmount =(totalBalancevalueresultvalo*saveinput)/100;
-//      const totalBalancevalueresult = totalBalancetype - avingAmount;
-//     console.log(totalBalancevalueresult)
-    
-// }
